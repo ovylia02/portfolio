0bd2f4e3d2dcd188aac429d0c105d1a822d507a7
Ovylia <ovylia02@gmail.com>
2024-07-22 18:03:14 +0700

    Added header for react app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Header from './components/header';
+import './App.css'
+import Header from './components/Header';
 
 function App() {
     return (
--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <header>
-            <nav>
-                {/* TODO: REPLACE TEXT WITH LOGO */}
-                <h1><a href="#home">Portfolio</a></h1>
-                <ul>
-                    {/* TODO: EDIT AND ADD MORE LINKS LATER */}
-                    <li><a href="#about">About</a></li>
-                </ul>
-            </nav>
-        </header>
+        <Navbar className="navbar" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img
+                        src={`${process.env.PUBLIC_URL}/logo.png`}
+                        alt="Logo"
+                        width="100"
+                        height="100"
+                        className="d-inline-block align-top"
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">.home</Nav.Link>
+                        <Nav.Link href="#">.placeholder</Nav.Link>
+                        {/* ADD MORE LINKS */}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
