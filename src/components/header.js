2c0672ac8c6d1a9ef4eb68d93a8c326f2c963e0d
Ovylia <ovylia02@gmail.com>
2024-07-22 12:23:53 +0700

    Implement react.js

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                {/* TODO: REPLACE TEXT WITH LOGO */}
+                <h1><a href="#home">Portfolio</a></h1>
+                <ul>
+                    {/* TODO: EDIT AND ADD MORE LINKS LATER */}
+                    <li><a href="#about">About</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
